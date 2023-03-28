ec0d7a1dfe88e6f478b43ab5573746cde937c117
cluab <cluab11222@gmail.com>
2023-03-28 13:01:45 +0200

    Add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './app/app';
+import { Provider } from 'react-redux';
+import { store } from './app/redux/greeting.slice';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={ store }>
+        <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+});
